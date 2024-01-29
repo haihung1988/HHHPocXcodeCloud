0604fc2f859ffd795530d070f529df18eb58d4e2
Hung Hoang <hai_hung_hoang@manulife.com>
2024-01-29 20:48:23 +0700

    Initial Commit

---
+++ HHHPocXcodeCloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HHHPocXcodeCloud
+//
+//  Created by Hung Hoang on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
